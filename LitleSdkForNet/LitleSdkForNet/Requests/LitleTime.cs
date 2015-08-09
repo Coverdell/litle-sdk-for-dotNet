5bdada3f9c311a680c6a83151993532ed7f43ac1
Tyler Kendrick <contact.tylerkendrick@gmail.com>
2015-08-08 23:03:25 -0500

    including untracked files from last commit

---
+++ LitleSdkForNet/LitleSdkForNet/Requests/LitleTime.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Litle.Sdk.Requests
+{
+    public class LitleTime
+    {
+        public virtual String GetCurrentTime(String format)
+        {
+            return DateTime.Now.ToString(format);
+        }
+    }
+}
