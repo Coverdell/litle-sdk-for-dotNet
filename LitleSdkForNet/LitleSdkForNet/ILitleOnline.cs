1f09002620bcfef33a27313b006207a962d0dccc
Tyler Kendrick <contact.tylerkendrick@gmail.com>
2015-08-08 23:05:16 -0500

    Put remaining classes into own files

---
+++ LitleSdkForNet/LitleSdkForNet/ILitleOnline.cs
@@ -0,0 +1,24 @@
+﻿using Litle.Sdk.Requests;
+using Litle.Sdk.Responses;
+
+namespace Litle.Sdk
+{
+    public interface ILitleOnline
+    {
+        AuthorizationResponse Authorize(Authorization auth);
+        AuthReversalResponse AuthReversal(AuthReversal reversal);
+        CaptureResponse Capture(Capture capture);
+        CaptureGivenAuthResponse CaptureGivenAuth(CaptureGivenAuth captureGivenAuth);
+        CreditResponse Credit(Credit credit);
+        EcheckCreditResponse EcheckCredit(EcheckCredit echeckCredit);
+        EcheckRedepositResponse EcheckRedeposit(EcheckRedeposit echeckRedeposit);
+        EcheckSalesResponse EcheckSale(EcheckSale echeckSale);
+        EcheckVerificationResponse EcheckVerification(EcheckVerification echeckVerification);
+        ForceCaptureResponse ForceCapture(ForceCapture forceCapture);
+        SaleResponse Sale(Sale sale);
+        RegisterTokenResponse RegisterToken(RegisterTokenRequestType tokenRequest);
+        LitleOnlineResponseTransactionResponseVoidResponse DoVoid(VoidTxn v);
+        LitleOnlineResponseTransactionResponseEcheckVoidResponse EcheckVoid(EcheckVoid v);
+        UpdateCardValidationNumOnTokenResponse UpdateCardValidationNumOnToken(UpdateCardValidationNumOnToken update);
+    }
+}
--- LitleSdkForNet/LitleSdkForNet/MethodOfPaymentTypeEnum.cs
+++ LitleSdkForNet/LitleSdkForNet/MethodOfPaymentTypeEnum.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.CodeDom.Compiler;
 using System.Xml.Serialization;
 
 namespace Litle.Sdk
 {
-    [GeneratedCode("xsd", "2.0.50727.42")]
     [Serializable]
     [XmlType(Namespace = "http://www.litle.com/schema")]
     public enum MethodOfPaymentTypeEnum
@@ -20,13 +18,5 @@ namespace Litle.Sdk
         EC,
         GC,
         [XmlEnum("")] Item,
-    }
-
-    public abstract class MethodOfPaymentSerializer
-    {
-        public static String Serialize(MethodOfPaymentTypeEnum mop)
-        {
-            return mop == MethodOfPaymentTypeEnum.Item ? "" : mop.ToString();
-        }
     }
 }
