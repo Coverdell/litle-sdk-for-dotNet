67101dfc8793df6fb954a19b42988267e337df2b
Tyler Kendrick <contact.tylerkendrick@gmail.com>
2015-08-08 23:05:49 -0500

    including untracked changed from last commit

---
+++ LitleSdkForNet/LitleSdkForNet/MethodOfPaymentSerializer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Litle.Sdk
+{
+    public abstract class MethodOfPaymentSerializer
+    {
+        public static String Serialize(MethodOfPaymentTypeEnum mop)
+        {
+            return mop == MethodOfPaymentTypeEnum.Item ? "" : mop.ToString();
+        }
+    }
+}
