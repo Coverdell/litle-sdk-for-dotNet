9477af058ddb1decd0e281a8dd2f8667b8e1a08d
Tyler Kendrick <contact.tylerkendrick@gmail.com>
2015-08-10 21:47:24 -0500

    neglected type mapping for extendedCardREsponseType

--- LitleSdkForNet/LitleSdkForNet/Responses/ExtendedCardResponseType.cs
+++ LitleSdkForNet/LitleSdkForNet/Responses/ExtendedCardResponseType.cs
@@ -2,7 +2,7 @@ using System.Xml.Serialization;
 
 namespace Litle.Sdk.Responses
 {
-    [XmlType(Namespace = "http://www.litle.com/schema")]
+    [XmlType("extendedCardResponseType", Namespace = "http://www.litle.com/schema")]
     public class ExtendedCardResponseType
     {
         private string _messageField;
