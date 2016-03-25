af3a74707c13e5b1de5d7ff9071bf7871369ca8c
Tyler Kendrick <tyler.kendrick@coverdell.com>
2016-03-25 13:58:59 -0500

    Removed nuget package.

--- LitleSdkForNet/LitleSdkForNet/Properties/AssemblyInfo.cs
+++ LitleSdkForNet/LitleSdkForNet/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("LitleSdkForNet")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("No description")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Vantiv")]
 [assembly: AssemblyProduct("LitleSdkForNet")]
